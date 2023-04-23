127f08e2f9f0d9e2464544faf424506daee49645
Asmaa <eng.asmaao2017@gmail.com>
2023-04-23 12:33:16 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+#include<stdio.h>
+int main(void)
+{
+	int n = 48;
+	int m = 49;
+
+	while (n < 57)
+	{
+		m = 49;
+		while (m < 58)
+		{
+			putchar(n);
+			putchar(m);
+			if (n < 57 || m < 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

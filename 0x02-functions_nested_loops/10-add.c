602ed04be51d69305aa8e7da908576bab023174a
Asmaa <eng.asmaao2017@gmail.com>
2023-04-25 19:00:31 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,14 @@
 /**
  * add - check the code
- *
+ * @n: int to be added to 
+ * @m: int to be added to n
  * Return: Always 0.
  */
 #include "main.h"
 int add(int n, int m)
 {
 	int r;
+
 	r = n + m;
 	return (r);
 }

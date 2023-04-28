1560a069c7f013e774718c82f343f28d1935b86b
Asmaa <eng.asmaao2017@gmail.com>
2023-04-27 17:30:02 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - check the code
+ * @a: int to be multyplt  to
+ * @b: int to be multiply to a
+ * Return: Always 0.
+ */
+#include "main.h"
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}

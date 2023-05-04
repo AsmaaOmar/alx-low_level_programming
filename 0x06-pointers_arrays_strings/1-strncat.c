d4f05add89d44b9f495a05b582616cbbed68f493
Asmaa <eng.asmaao2017@gmail.com>
2023-05-04 16:36:34 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings with an intrgrt
+ * @dest: input value
+ * @src: input value
+ * @n: interger counter
+ * Return: void
+ */
+ char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}

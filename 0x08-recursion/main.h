487982bcea5d988648ed2daa915fbe6e8ac8d78e
Asmaa <eng.asmaao2017@gmail.com>
2023-05-11 18:02:31 -0700

    100-is palindrom

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int natural_square(int n, int num);
+int is_palindrome(char *s);
+
 #endif
